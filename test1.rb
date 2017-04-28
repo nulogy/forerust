e87ee356d32dd0a20c19f7b52139ffceec01c767
Justin Fitzsimmons <justinf@nulogy.com>
2017-04-28 15:59:00 -0400

    Captures and prints stderr

--- test1.rb
+++ test1.rb
@@ -2,8 +2,8 @@
 
 $stdout.sync = true
 
+STDERR.puts "Hello from STDERR!"
+puts "Environment variable FOO: #{ENV['FOO']}"
 puts "Hello from STDOUT test 1!"
 sleep 2
 puts "Hello test 1 again"
-puts "Environment variable FOO: #{ENV['FOO']}"
-STDERR.puts "Hello from STDERR!"
